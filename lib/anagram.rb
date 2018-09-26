0087115adb0040166ba5eed66e902466482ec320
DonovanDwyer <dwyer.donovan@gmail.com>
2018-09-26 16:59:04 -0400

    build anagram detection engine

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require "pry"
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    #binding.pry
+    anagrams.select do |anagram|
+      anagram.split("").sort == @word.split("").sort
+    end
+  end
+
+end
